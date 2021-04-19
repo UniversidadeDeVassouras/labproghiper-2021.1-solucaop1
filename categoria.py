686edff54e4cb2a92e20b4e8350351da05194c5f
Tássio Auad <auadtassio@gmail.com>
2021-04-19 20:19:02 -0300

    Criadas as entidades

---
+++ categoria.py
@@ -0,0 +1,18 @@
+from typing import List
+from video import Video
+
+class Categoria:
+
+    def __int__(self, id: int, nome: str, video_list: List[Video]):
+        self.__id = id
+        self.__nome = nome
+        self.__video_list = video_list
+    
+    def get_id(self) -> int:
+        return self.__id
+
+    def get_nome(self) -> str:
+        return self.__nome
+    
+    def get_video_list(self) -> List[Video]
+        return self.__video_list
---
+++ video.py
@@ -0,0 +1,23 @@
+class Video:
+
+    def __init__(self, id: int, titulo: str, descricao:str, imagem_path: str, video_path: str):
+        self.__id = id
+        self.__titulo = titulo
+        self.__descricao = descricao
+        self.__imagem_path = imagem_path
+        self.__video_path = video_path
+
+    def get_id(self) -> int:
+        return self.__id
+    
+    def get_titulo(self) -> str:
+        return self.__titulo
+    
+    def get_descricao(self) -> str:
+        return self.__descricao
+
+    def get_imagem_path(self) -> str:
+        return self.__imagem_path
+
+    def get_video_path(self) -> str:   
+        return self.__video_path
