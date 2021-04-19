cdbf1a80c8b83219b27edee8ed7232b34c609ddc
Tássio Auad <auadtassio@gmail.com>
2021-04-19 20:05:26 -0300

    Home Skeleton
    Criado o skeleton da página home

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Aloha!</h1>"
-
-@app.route("/ola")
-def ola():
-    return "<h1>Olá, mundo!</h1>"
+    return render_template("index.html")
