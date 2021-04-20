bf4c0668b83bdd07bff5af965fe2969fc246cba4
Tássio Auad <auadtassio@gmail.com>
2021-04-19 22:01:24 -0300

    Perfil do Vídeo
    Criada a página de perfil de vídeos onde é reproduzido o vídeo de fato.

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from categoria import Categoria
 
 app = Flask(__name__)
 
-video_streetfighter = Video(1, 'Street Fighter', 'Vídeo do Street Fighter', 'img/stf1.jpg', 'video/stf1.mp4')
-video_naturo = Video(2, 'Naturo', 'Vídeo de Naruto', 'img/shonen1.jpg', 'video/shonen1.webm')
-video_desaf_gigantes = Video(3, 'Desafiando Gigantes', 'Vídeo Desafiando Gigantes', 'img/capa_desafiandogigantes.png', 'video/desafiando-gigantes.mp4')
-video_mr_sunshine = Video(4, 'Mr. Sunshine', 'Vídeo Mr. Sunshine', 'img/capa_mrsunshine.png', 'video/desafiando-gigantes.mp4')
+video_streetfighter = Video(1, 'Street Fighter', 'Vídeo do Street Fighter', 'img/stf1.jpg', 'video/stf1.mp4', 'video/mp4')
+video_naturo = Video(2, 'Naturo', 'Vídeo de Naruto', 'img/shonen1.jpg', 'video/shonen1.webm', 'video/webm')
+video_desaf_gigantes = Video(3, 'Desafiando Gigantes', 'Vídeo Desafiando Gigantes', 'img/capa_desafiandogigantes.png', 'video/desafiando-gigantes.mp4', 'video/mp4')
+video_mr_sunshine = Video(4, 'Mr. Sunshine', 'Vídeo Mr. Sunshine', 'img/capa_mrsunshine.png', 'video/mr-sunshine.mp4', 'video/mp4')
 
 categoria_anime = Categoria(1, 'Animes', [video_streetfighter, video_naturo])
 categoria_filme_inter = Categoria(2, 'Filmes Internacionais', [video_mr_sunshine, video_desaf_gigantes])
@@ -17,3 +17,12 @@ categorias = [categoria_anime, categoria_filme_inter]
 @app.route("/")
 def home():
     return render_template("home.html", categoria_list = categorias)
+ 
+@app.route("/video/<int:id>")
+def video(id: int):
+    for categoria in categorias:
+        for video in categoria.get_video_list():
+            if video.get_id() == id:
+                return render_template("video.html", video=video)
+    return render_template("video.html")
+
--- video.py
+++ video.py
@@ -1,11 +1,12 @@
 class Video:
     
-    def __init__(self, id: int, titulo: str, descricao:str, imagem_path: str, video_path: str):
+    def __init__(self, id: int, titulo: str, descricao:str, imagem_path: str, video_path: str, mime: str):
         self.__id = id
         self.__titulo = titulo
         self.__descricao = descricao
         self.__imagem_path = imagem_path
         self.__video_path = video_path
+        self.__mime = mime
 
     def get_id(self) -> int:
         return self.__id
@@ -21,3 +22,6 @@ class Video:
 
     def get_video_path(self) -> str:   
         return self.__video_path
+
+    def get_mime(self) -> str:
+        return self.__mime
